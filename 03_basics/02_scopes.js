afd278b0aabf9a9761b064f2c4d70a221de313de
Samarth Kulkarni <118709050+striver3-14-15@users.noreply.github.com>
2023-12-03 13:59:30 +0000

    Learnt scopes in functions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -27,32 +27,32 @@ function one(){
 
 }
 
-// one()
+one()
 
-if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    // console.log(website);
-}
+// if (true) {
+//     const username = "hitesh"
+//     if (username === "hitesh") {
+//         const website = " youtube"
+//         // console.log(username + website);
+//     }
+//     // console.log(website);
+// }
 
-// console.log(username);
-
-
-// ++++++++++++++++++ interesting ++++++++++++++++++
+// // console.log(username);
 
 
-console.log(addone(5))
-
-function addone(num){
-    return num + 1
-}
+// // ++++++++++++++++++ interesting ++++++++++++++++++
 
 
+// console.log(addone(5))
 
-addTwo(5)
-const addTwo = function(num){
-    return num + 2
-}
+// function addone(num){
+//     return num + 1
+// }
+
+
+
+// addTwo(5)
+// const addTwo = function(num){
+//     return num + 2
+// }
