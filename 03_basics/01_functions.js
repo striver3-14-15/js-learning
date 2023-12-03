9eea11c42165ea8363f68932d12cafe63ff77855
Samarth Kulkarni <118709050+striver3-14-15@users.noreply.github.com>
2023-12-03 13:15:27 +0000

    ---

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -55,17 +55,18 @@ console.log(calculateCartPrice(200, 400, 500, 2000))
 //     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 // }
 
-// // handleObject(user)
+// handleObject(user)
 // handleObject({
 //     username: "sam",
 //     price: 399
 // })
 
-// const myNewArray = [200, 400, 100, 600]
+const myNewArray = [200, 400, 100, 600]
 
-// function returnSecondValue(getArray){
-//     return getArray[1]
-// }
+function returnSecondValue(getArray){
+    return getArray[1]
+}
 
-// // console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue(myNewArray));
 // console.log(returnSecondValue([200, 400, 500, 1000]));
