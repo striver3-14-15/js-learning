c7b99f139196921477dee1626dd6cf76993e9bef
Samarth Kulkarni <118709050+striver3-14-15@users.noreply.github.com>
2023-12-03 14:08:35 +0000

    works only on objects, not on function variables

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,8 +3,8 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(`Hello ${this.username} , welcome to website`);
+        console.log(this);  //this returns entire json 
     }
 
 }
@@ -13,24 +13,25 @@ const user = {
 // user.username = "sam"
 // user.welcomeMessage()
 
-// console.log(this);
+// console.log(this);  //it gives blank `{}`
 
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
+function chai(){
+    let username = "siuuu"
+    console.log(this.username); //output : undefined
+    // console.log(this)
+}
 
-// chai()
+chai()
 
 // const chai = function () {
 //     let username = "hitesh"
 //     console.log(this.username);
 // }
 
-const chai =  () => {
-    let username = "hitesh"
-    console.log(this);
-}
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this);
+// }
 
 
 // chai()
@@ -43,10 +44,10 @@ const chai =  () => {
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// const addTwo = (num1, num2) => ({username: "hitesh"})
 
 
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))
 
 
 // const myArray = [2, 5, 3, 7, 8]
